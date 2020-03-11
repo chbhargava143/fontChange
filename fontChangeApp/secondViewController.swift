f0b44215559659ec81b6d77f5c2cab37431f67a0
Bhargava_mike <saibhargava54@gmail.com>
2020-03-11 11:39:15 +0530

    story board creation

---
+++ fontChangeApp/secondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  secondViewController.swift
+//  fontChangeApp
+//
+//  Created by chbn on 11/03/20.
+//  Copyright © 2020 chbn. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
